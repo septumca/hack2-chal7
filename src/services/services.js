c27e93a75ffe0d8ffbbf244d81574d1c97fe7a55
Karol Kocis <karol.kocis@labourdigital.de>
2020-11-21 05:27:21 +0100

    repair commit

--- src/services/services.js
+++ src/services/services.js
@@ -1,5 +1,15 @@
 import axios from 'axios';
 
+export const getAllItems = async () => {
+  const { data } = await axios.get(`${process.env.REACT_APP_BE_BASE}/items`);
+  return data;
+}
+
+export const getItemData = async (id=null) => {
+  const { data } = await axios.get(`${process.env.REACT_APP_BE_BASE}/items/${id}`);
+  return data;
+}
+
 export const getAccounts = async (name=null) => {
   const { data } = await axios.get(`${process.env.REACT_APP_BE_BASE}/accounts/${name}`);
   return data;
