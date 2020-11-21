8e8e6a58f3bdefedf3c7ab59e2c7034b47d50ff0
Ofir Katz <ofirkatz94@gmail.com>
2020-11-21 10:33:08 +0100

    fix: link duplicate

--- src/components/BottomAppBar.js
+++ src/components/BottomAppBar.js
@@ -7,7 +7,7 @@ import AddAlertIcon from '@material-ui/icons/AddAlert';
 import SettingsIcon from '@material-ui/icons/Settings';
 import SearchIcon from '@material-ui/icons/Search';
 import Button from '@material-ui/core/Button';
-import {Link} from 'react-router-dom';
+
 const useStyles = makeStyles(() => ({
 
     icon: {
