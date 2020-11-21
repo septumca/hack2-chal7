46783592c09f4be49dd92c67652f0d7b31fa48df
Karol Kocis <karol.kocis@labourdigital.de>
2020-11-21 04:48:53 +0100

    part of services methods for BE

--- src/services/services.js
+++ src/services/services.js
@@ -1,12 +1,27 @@
 import axios from 'axios';
 
-
-export const getAllItems = async () => {
-  const { data } = await axios.get(`${process.env.REACT_APP_BE_BASE}/items`);
+export const getAccounts = async (name=null) => {
+  const { data } = await axios.get(`${process.env.REACT_APP_BE_BASE}/accounts/${name}`);
   return data;
 }
 
-export const getItemData = async (id=null) => {
-  const { data } = await axios.get(`${process.env.REACT_APP_BE_BASE}/items/${id}`);
+export const getAbilities = async () => {
+  const { data } = await axios.get(`${process.env.REACT_APP_BE_BASE}/abilities`);
   return data;
+}
+
+export const postAccount = (data) => {
+  let payload = { ...data };
+   return axios.post(`${process.env.REACT_APP_BE_BASE}/accounts`, payload).then(({ status, data }) => {
+    console.log(data);
+    return {...data, status: data.status}
+  });
+}
+
+export const postAbilities = (data) => {
+  let payload = { ...data };
+   return axios.post(`${process.env.REACT_APP_BE_BASE}/abilities`, payload).then(({ status, data }) => {
+    console.log(data);
+    return {...data, status: data.status}
+  });
 }
